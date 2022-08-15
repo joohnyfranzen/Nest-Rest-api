4faaa7fe7a226fc0f995c1a64f6e8b06d49fff3b
Your Name <you@example.com>
2022-08-15 13:56:39 -0300

    Signup function added, with hash  and other funct

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,12 +5,22 @@ import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) {
+    constructor(private prisma: PrismaService) {}
+    
+    async signup(dto: AuthDto) {
+        // generate password hash
+        const hash = await argon.hash(dto.password);
+        // save the new user in db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+        });
+        delete user.hash;
+        // return saved user
 
-    }
-
-    signup(dto: AuthDto) {
-        return { msg: 'I am signed up'};
+        return user;
     }
 
     signin() {
