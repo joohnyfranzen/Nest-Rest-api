4f4087e81f5d2cbbdc1109bca42bee9d29003397
Your Name <you@example.com>
2022-08-15 14:08:42 -0300

    Prisma duplicated error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable({})
 export class AuthService {
@@ -11,16 +12,26 @@ export class AuthService {
         // generate password hash
         const hash = await argon.hash(dto.password);
         // save the new user in db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
-        delete user.hash;
-        // return saved user
-
-        return user;
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            delete user.hash;
+            // return saved user
+    
+            return user;
+            
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credenciais em uso',);
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
