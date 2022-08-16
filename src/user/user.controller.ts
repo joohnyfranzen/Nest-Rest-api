e84019f65dfc70f1d58124281c505c3ae84455a2
Your Name <you@example.com>
2022-08-16 11:30:49 -0300

    User Controller Update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@Req() req: Request) {
-        return req.user;
+    getMe(
+        @GetUser() user: User,
+        @GetUser('email') email: string,
+        ) {
+            console.log({
+                email,
+            })
+        return user;
+    }
+    @Patch()
+    editUser(){
     }
 }
