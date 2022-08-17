6f21ab1029edf6a1b8dc53a36e8785b3b7446dbe
Your Name <you@example.com>
2022-08-17 12:17:51 -0300

    User COntroller Edit User

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { Request } from 'express';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {}
     @Get('me')
     getMe(
         @GetUser() user: User,
@@ -18,6 +21,9 @@ export class UserController {
         return user;
     }
     @Patch()
-    editUser(){
+    editUser(
+        @GetUser('id') userId: number, 
+        @Body() dto: EditUserDto,){
+            return this.userService.editUser(userId, dto)
     }
 }
